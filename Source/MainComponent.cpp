2a17794a73be13d03c78b105ce94799468df23e0
UlrikKowalk <ulrik.kowalk@jade-hs.de>
2021-02-10 00:31:51 +0100

    Somethng's working.

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,11 +3,20 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+
+    m_formatManager.registerBasicFormats();
+
+    m_ui_load.setButtonText("Load File");
+    m_ui_load.addListener(this);
+    addAndMakeVisible(m_ui_load);
+
     setSize (600, 400);
 }
 
 MainComponent::~MainComponent()
 {
+
+
 }
 
 //==============================================================================
@@ -16,9 +25,25 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    m_ui_load.setBounds(10, 10, 60, 20);
+
+    if (m_sample != 0) {
+        float wid = ((float) getWidth() - 20) / m_numBlocks;
+        float hei = ((float) getHeight() - 80) / m_numChannels;
+
+        for (int iBlock = 0; iBlock < m_numBlocks; iBlock++) {
+            for (int iChannel = 0; iChannel < m_numChannels; iChannel++) {
+
+                unsigned char col = (m_levels.at(iChannel).at(iBlock) - minLevel) * 255 / (maxLevel - minLevel);
+                g.setColour(juce::Colour(col, col, col, 1.0f));
+
+                juce::Rectangle<float> tmp(10 + iBlock * wid, 40 + iChannel * hei, wid, hei);
+                g.fillRect(tmp);
+
+            }
+        }
+    }
+
 }
 
 void MainComponent::resized()
@@ -26,4 +51,109 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+
+}
+
+void MainComponent::callbackOpen()
+{
+
+    m_buff.clear();
+
+    //choose a file
+    juce::FileChooser chooser("Choose a Wav or AIFF File", juce::File::getSpecialLocation(juce::File::userDesktopDirectory), "*.wav; *.mp3");
+
+    //if the user chooses a file
+    if (chooser.browseForFileToOpen())
+    {
+        juce::File myFile;
+
+        //what did the user choose?
+        myFile = chooser.getResult();
+
+        //read the file
+        juce::AudioFormatReader* reader = m_formatManager.createReaderFor(myFile);
+
+        if (reader != nullptr)
+        {
+            //get the file ready to play
+            std::unique_ptr<juce::AudioFormatReaderSource> tempSource(new juce::AudioFormatReaderSource(reader, true));
+
+            m_transport.setSource(tempSource.get());
+            m_playSource.reset(tempSource.release());
+            m_samplerate = reader->sampleRate;
+            m_numSamples = reader->lengthInSamples;
+            m_numChannels = reader->numChannels;
+            m_sample = 0;
+            m_numBlocks = floor(m_numSamples / BUFFER_SIZE);
+            m_block = 0;
+
+            m_buff.setSize((int)reader->numChannels, (int)reader->lengthInSamples); 
+            reader->read(&m_buff, 0, m_numSamples, 0, true, true);
+
+            m_levels.resize(m_numChannels);
+            for (int iChannel = 0; iChannel < m_numChannels; iChannel++) {
+                m_levels.at(iChannel).resize(m_numBlocks, 0.0f);
+            }
+
+            loop();
+        }
+    }
+   
+}
+
+void MainComponent::loop() {
+    while (m_sample < m_numSamples - BUFFER_SIZE) {
+    
+
+
+        //DBG(m_buffer.buffer->getNumSamples());
+
+        for (int iChannel = 0; iChannel < m_numChannels; iChannel++) {
+            const float* tmp = m_buff.getReadPointer(iChannel, m_sample);
+            m_levels.at(iChannel).at(m_block) = rmsLevel(tmp, BUFFER_SIZE);
+
+            if (m_levels.at(iChannel).at(m_block) < minLevel) {
+                minLevel = m_levels.at(iChannel).at(m_block);
+            }
+            else if (m_levels.at(iChannel).at(m_block) > maxLevel) {
+                maxLevel = m_levels.at(iChannel).at(m_block);
+            }
+        }
+
+        m_sample += BUFFER_SIZE;
+        m_block++;
+        
+    }
+
+    repaint();
+    DBG("finished.");
+}
+
+
+
+void MainComponent::process() {
+    //File file("d:/my.wav");
+    //AudioFormatManager formatManager;
+    //formatManager.registerBasicFormats();
+    //ScopedPointer<AudioFormatReader> reader = formatManager.createReaderFor(file);
+    //if (reader != 0)
+    {
+        //AudioSampleBuffer buffer(reader->numChannels, reader->lengthInSamples);
+        //buffer.read(reader, 0, reader->lengthInSamples, 0, true, true);
+        //float* firstChannelSamples = buffer.getSampleData(0, 0);
+    }
+}
+
+double MainComponent::rmsLevel(const float* inBuffer, int numSamples) {
+
+    double sum = 0;
+
+    for (int iSample = 0; iSample < numSamples; iSample++) {
+        sum += inBuffer[iSample] * inBuffer[iSample];
+    }
+    sum /= numSamples;
+    sum = sqrt(sum);
+    sum = juce::Decibels::gainToDecibels(sum);
+
+    return sum;
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,13 +1,16 @@
 #pragma once
 
-#include <JuceHeader.h>
+#include "Constants.h"
+#include "JuceHeader.h"
+
 
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::Component
+class MainComponent  : public juce::Component,
+                       public juce::Button::Listener
 {
 public:
     //==============================================================================
@@ -19,9 +22,37 @@ public:
     void resized() override;
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
 
+    void MainComponent::buttonClicked(juce::Button* button) {
+        if (button == &m_ui_load) {
+            callbackOpen();
+        }
+    }
+
+    void MainComponent::callbackOpen();
+    void MainComponent::loop();
+    void MainComponent::process();
+    double MainComponent::rmsLevel(const float* inBuffer, int numSamples);
+
+    juce::TextButton m_ui_load;
+
+    int m_samplerate = 0;
+    int m_numSamples = 0;
+    int m_numChannels = 0;
+    int m_sample = 0;
+    int m_numBlocks = 0;
+    int m_block = 0;
+
+    float minLevel = 100; 
+    float maxLevel = 0;
+
+    std::vector<std::vector<float>> m_levels;
+
+    juce::AudioFormatManager m_formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> m_playSource;
+    juce::AudioTransportSource m_transport;
+
+    juce::AudioSampleBuffer m_buff;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
